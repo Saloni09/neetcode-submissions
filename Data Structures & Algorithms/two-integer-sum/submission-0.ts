83af05f205b278d3c881c35ce6f93d5818c3c211
Saloni09 <saloniv2409@gmail.com>
2026-09-13 10:36:50 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        let mp = new Map();
+        for(let i=0;i<nums.length;i++){
+            if(mp.has(target-nums[i]))return [i, mp.get(target-nums[i])]
+            mp.set(nums[i],i)
+        }
+    }
+}
